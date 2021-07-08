ca4b733fa9dd5bf2f1f96ebce6b0b32f7ebbba0b
Angel Villena <avillena@sdi.es>
2021-07-08 17:40:56 +0200

    Añadido JS y botón en index.html

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("functions.js ready!!!");  
+
+    document.getElementById("btn_clicame").onclick = function(){
+        alert("Giteando eeh?");
+    };
+});
